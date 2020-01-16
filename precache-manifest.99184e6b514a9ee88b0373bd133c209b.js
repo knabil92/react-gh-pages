3930ffa6a2dde8c30b9b7836a6ecf7ff1caae7e3
Khaled Nabil <kn2016a@gmail.com>
2020-01-16 12:14:38 +0200

    Updates

---
+++ precache-manifest.99184e6b514a9ee88b0373bd133c209b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb37b55ae456ea1a7f44f9f38160e9fd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "cd3f6a020d91ea006c89",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa86d99106fd62954a28",
+    "url": "/react-gh-pages/static/js/2.47901a3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.47901a3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd3f6a020d91ea006c89",
+    "url": "/react-gh-pages/static/js/main.2b63c50f.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
